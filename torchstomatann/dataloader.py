44954907a2fdda09349580be79e19b8bcfb35297
AidanSBrown <aidansbrown@gwu.edu>
2024-12-22 18:00:24 -0500

    Expanding model architecture to be similar to UNET for eventual segment detection

--- torchstomatann/dataloader.py
+++ torchstomatann/dataloader.py
@@ -5,10 +5,9 @@ from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torchvision import transforms, utils, datasets
 
 points_frame = pd.read_csv('data/faces/face_landmarks.csv')
-
 
 def show_landmarks(image, landmarks):
     """Show image with landmarks"""
@@ -16,11 +15,69 @@ def show_landmarks(image, landmarks):
     plt.scatter(landmarks[:, 0], landmarks[:, 1], s=10, marker='.', c='r')
     plt.pause(0.001)  # pause a bit so that plots are updated
 
-for n, row in points_frame.iterrows():
-    img_name = points_frame.iloc[n, 0]
-    landmarks = points_frame.iloc[n, 1:]
-    landmarks = np.asarray(landmarks, dtype=float).reshape(-1, 2)
-    plt.figure()
-    show_landmarks(io.imread(os.path.join('data/faces/', img_name)),
-               landmarks)
-    plt.show()
+
+class StomataDataset(Dataset):
+    """Stomata Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Arguments:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.annotations.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.annotations.iloc[idx, 1:]
+        landmarks = np.array([landmarks], dtype=float).reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        if self.transform:
+            image = self.transform(image)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        return sample
+
+data_transform = transforms.Compose([
+        transforms.Resize(256,256),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+dataset = StomataDataset(csv_file='data/faces/face_landmarks.csv',
+                                    root_dir='data/faces/',
+                                    transforms = data_transform)
+
+fig = plt.figure()
+
+for i, sample in enumerate(dataset):
+    print(i, sample['image'].shape, sample['landmarks'].shape)
+
+    ax = plt.subplot(1, 4, i + 1)
+    plt.tight_layout()
+    ax.set_title('Sample #{}'.format(i))
+    ax.axis('off')
+    show_landmarks(**sample)
+
+    if i == 3:
+        plt.show()
+        break
+
+
+
+
--- torchstomatann/main.py
+++ torchstomatann/main.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import numpy as np
+import torch.nn.functional as F
 
 # Set GPU or CPU
 device = (
@@ -19,22 +20,59 @@ print(f"Using {device}")
 class NerualNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
+        # Encoder
+        self.enc1 = self.conv_block(3, 64)  # 3 channels (RGB)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
+
+        # Bottleneck
+        self.center = self.conv_block(512, 1024)
+
+        # Decoder
+        self.dec4 = self.conv_block(1024, 512)
+        self.dec3 = self.conv_block(512, 256)
+        self.dec2 = self.conv_block(256, 128)
+        self.dec1 = self.conv_block(128, 64)
+
+        # Final output layer (Binary mask)
+        self.final = nn.Conv2d(64, 1, kernel_size=1)
+    
+    def conv_block(self, in_channels, out_channels): # Conv block is for repeatability when having multiple convolutionary layers
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
         )
     
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(F.max_pool2d(enc1, 2))
+        enc3 = self.enc3(F.max_pool2d(enc2, 2))
+        enc4 = self.enc4(F.max_pool2d(enc3, 2))
+
+        # Bottleneck
+        center = self.center(F.max_pool2d(enc4, 2))
+
+        # Decoder path
+        dec4 = self.dec4(F.upsample(center, scale_factor=2, mode='bilinear', align_corners=True))
+        dec4 = dec4 + enc4  # Skip connection
+
+        dec3 = self.dec3(F.upsample(dec4, scale_factor=2, mode='bilinear', align_corners=True))
+        dec3 = dec3 + enc3
+
+        dec2 = self.dec2(F.upsample(dec3, scale_factor=2, mode='bilinear', align_corners=True))
+        dec2 = dec2 + enc2
+
+        dec1 = self.dec1(F.upsample(dec2, scale_factor=2, mode='bilinear', align_corners=True))
+        dec1 = dec1 + enc1
+
+        output = torch.sigmoid(self.final(dec1))
+        return output
 
 model = NerualNetwork().to(device)
-# print(model)
+print(model)
 
 X = torch.rand(1, 28, 28, device=device) # Creates a tensor of random image / data
 
@@ -42,10 +80,7 @@ X = torch.rand(1, 28, 28, device=device) # Creates a tensor of random image / da
 # stomata_image = torch.tensor(data)
 
 
-
-logits = model(X)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
-
+params = list(model.parameters())
+print(len(params))
+print(params[0].size())  
 
